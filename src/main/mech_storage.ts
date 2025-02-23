74dbaf2e135b624b51604f659e57e3ff5ec43aa5
Antti Kautiainen <antti@kautiainen.com>
2025-02-23 21:00:47 +0200

    doc: class SimpleStorage
    Changes to be committed:
    	modified:   src/main/mech_storage.ts
        - doc: class SimpleStorage
        - feat: optional support for firing events for parts and stored changes.
        - feat: bays proxy option.
        - feat: opitonal support for firing events for mech bay changes.

--- src/main/mech_storage.ts
+++ src/main/mech_storage.ts
@@ -5,7 +5,7 @@
  */
 
 import BTechGameModel from "./index.ts";
-import { Count, createCount, createIntCount, Mech, MechBay } from "./mech.ts";
+import { AssembledMech, Count, createCount, createIntCount, Loadout, Mech, MechBay } from "./mech.ts";
 
 /**
  * The mech storage. 
@@ -41,12 +41,26 @@ export class SimpleStorage implements MechStorage {
 
     private myName: string;
 
+    /**
+     * The mapping from a model name to the model name or mech count.
+     */
     private myParts: Map<string, Count<string | Mech>> = new Map();
 
+    /**
+     * The mapping from a model name to the model name or mech count.
+     */
     private myStored: Map<string, Count<string | Mech>> = new Map();
-    
-    
-    
+
+    /**
+     * The mech bays of the storage. 
+     */
+    private myBays : MechBay[] = [];
+
+    /**
+     * Create a new simple storage.
+     * @param name The name of the storage.
+     * @param options The storage options. 
+     */
     constructor(name: string, options?: Partial<{ parts: Iterable<Count<string | Mech>>, stored: Iterable<Count<string | Mech>> }>) {
         this.myName = name;
         if (options) {
@@ -54,7 +68,7 @@ export class SimpleStorage implements MechStorage {
                 for (let count of options.parts) {
                     switch (typeof count.value) {
                         case "string":
-                            this.addParts(count.value, count.count); 
+                            this.addParts(count.value, count.count);
                             break;
                         default:
                             // Mech.
@@ -66,7 +80,7 @@ export class SimpleStorage implements MechStorage {
                 for (let count of options.stored) {
                     switch (typeof count.value) {
                         case "string":
-                            this.addStored(count.value, count.count); 
+                            this.addStored(count.value, count.count);
                             break;
                         default:
                             // Mech.
@@ -84,6 +98,65 @@ export class SimpleStorage implements MechStorage {
     set name(newName: string) {
         this.myName = newName;
     }
+
+
+    /**
+     * Fire parts chagned event to the listeners. 
+     * @param model The model changed.
+     * @param newCount The new count of parts. An undefined value indicates
+     * the model is removed from the parts.
+     */
+    firePartsChanged(model: string, newCount: Count<string | Mech>|undefined) {
+
+    }
+
+    /**
+     * Fire stored chagned event to the listeners. 
+     * @param model The model changed.
+     * @param newCount The new count of stored chassis. An undefined value indicates
+     * the model is removed from the stored chassis.
+     */
+    fireStoredChanged(model: string, newCount: Count<string | Mech>|undefined) {
+
+    }
+
+    /**
+     * Fire event indicating a mech bay content has changed.  
+     * @param target The changed hangar bay. 
+     * @param slot The mech bay slot.
+     * @param mech The new mech in the slot. If undefined, the slot is relieved.
+     * @param loadout The loadout change. If the mech is undefined, the loadout is added to the 
+     * stores, and otherwise the loadout is assembled to the mech. If undefined, there is no 
+     * equipoment change. 
+     * 
+     * @param newCount The new count of parts.
+     */
+    fireMechBayChanged(target : MechBay, slot: number, mech: AssembledMech|undefined, loadout: Loadout|undefined) {
+
+    }
+
+    /**
+     * Fire event indicating a mech is assembled to an empty slot.
+     * @param target The target bay.
+     * @param slot The slot of the bay.
+     * @param mech The assembled mech.
+     * @param loadout The loadout equipped to the mech from equipment stores.
+     */
+    fireMechAssembled(target: MechBay, slot: number, mech: AssembledMech, loadout: Loadout) {
+        this.fireMechBayChanged(target, slot, mech, loadout);
+    }
+
+    /**
+     * Fire event indicating a mech is stored from its slot. 
+     * @param target The target bay.
+     * @param slot The slot of the mech.
+     * @param mech The mech removed from the slot.
+     */
+    fireMechStored(target: MechBay, slot: number, mech: AssembledMech) {
+        this.fireMechBayChanged(target, slot, undefined, mech.loadout);
+    }
+
+
 
     /**
      * Get the part count.
@@ -122,14 +195,32 @@ export class SimpleStorage implements MechStorage {
             throw new RangeError("The part count is invalid");
         }
         let current = this.myParts.get(model);
+        let newCount: Count<string | Mech> | undefined;
         if (current) {
-            this.myParts.set(model, current.set(count));
-            return current;
+            this.myParts.set(model, newCount = current.set(count));
         } else {
             // Adding a new model
-            this.myParts.set(model, createIntCount(model, count, { min: 0 }));
-            return undefined;
+            this.myParts.set(model, newCount = createIntCount(model, count, { min: 0 }));
         }
+
+        /**
+         * @todo Split the new count between stored chassis and parts.
+         */
+        /**
+         * New stored amount calculated from the current stored amount. 
+         */
+        const newStored : Count<string | Mech>|undefined = undefined;
+
+        /**
+         * @todo fire events.
+        */
+        this.firePartsChanged(model, newCount);
+        if (newStored) {
+            this.setStored(model, newStored);
+        }
+
+        return current;
+
     }
 
     /**
@@ -138,12 +229,14 @@ export class SimpleStorage implements MechStorage {
      * @param delta The mount of parts added. 
      */
     addParts(model: string, delta: number) {
-        const current : Count<string | Mech>|undefined = this.myParts.get(model);
+        const current: Count<string | Mech> | undefined = this.myParts.get(model);
         if (current) {
             this.setParts(model, current.add(delta).count);
         } else {
             this.setParts(model, delta);
         }
+
+
     }
 
     /**
@@ -152,13 +245,16 @@ export class SimpleStorage implements MechStorage {
      * @param count The amoutn of parts removed.
      */
     removeParts(model: string, count: number) {
-        const current : Count<string | Mech>|undefined = this.myParts.get(model);
+        const current: Count<string | Mech> | undefined = this.myParts.get(model);
         if (current) {
             this.setParts(model, current.add(-count).count);
         } else {
             this.setParts(model, -count);
         }
 
+        /**
+         * @todo fire event.
+         */
     }
 
     /**
@@ -180,6 +276,11 @@ export class SimpleStorage implements MechStorage {
     deleteStored(model: string): number | undefined {
         const result = this.getStored(model)?.count;
         if (this.myStored.delete(model)) {
+
+            /**
+             * @todo fire events.
+             */
+            this.fireStoredChanged(model, undefined);
             return result;
         } else {
             return undefined;
@@ -193,35 +294,40 @@ export class SimpleStorage implements MechStorage {
      * @returns The replaced stored chassis count.
      * @throws {RangeError} The count is invalid.
      */
-    setStored(model: string, count: number|Count<string | Mech>): Count<string | Mech> | undefined {
+    setStored(model: string, count: number | Count<string | Mech>): Count<string | Mech> | undefined {
         if (typeof count === "number" && !(Number.isSafeInteger(count) || count < 0)) {
             throw new RangeError("The part count is invalid");
         }
         let current = this.myStored.get(model);
+        let newCount : Count<string | Mech>|undefined;
         if (current) {
             if (typeof current.value !== "string" || typeof count === "number" || typeof count.value === "string"
                 || count.value.model !== model
             ) {
                 // The current value is a mech or the count does not have proper mech as its value
                 // - creating a new value by deriving a count from the current count. 
-                this.myStored.set(model, current.set(typeof count === "number" ? count : count.count));
+                this.myStored.set(model, newCount = current.set(typeof count === "number" ? count : count.count));
             } else {
                 // The current value is a count of model name, and the new count contains a valid mech.
                 // - Creating a new count with count mech as its value.
-                this.myStored.set(model, createIntCount(count.value, count.count, {min: 0}));
+                this.myStored.set(model, newCount = createIntCount(count.value, count.count, { min: 0 }));
             }
-            return current;
         } else if (typeof count === "number") {
             // The count is a number, thus using the model name for counted value.
-            this.myStored.set(model, createIntCount(model, count, { min: 0 }));
-        } else if ( (typeof count.value === "string" ? count.value : count.value.model) === model) {
+            this.myStored.set(model, newCount = createIntCount(model, count, { min: 0 }));
+        } else if ((typeof count.value === "string" ? count.value : count.value.model) === model) {
             // The count.valu is either a valid mech with proper model name or a valid model name. Using it as counted value.
-            this.myStored.set(model, createIntCount(count.value, count.count, {min: 0}));
+            this.myStored.set(model, newCount = createIntCount(count.value, count.count, { min: 0 }));
         } else {
             // The count does not have a valid mech or mech name, and thus only using the count.
-            this.myStored.set(model, createIntCount(model, count.count, {min: 0}));
+            this.myStored.set(model, newCount = createIntCount(model, count.count, { min: 0 }));
         }
-        return undefined; 
+
+        // Fire event.
+        this.fireStoredChanged(model, newCount);
+
+        // Returning the previous value.
+        return current;
     }
 
     /**
@@ -231,7 +337,7 @@ export class SimpleStorage implements MechStorage {
      */
     addStored(model: string, delta: number) {
         if (delta < 0) throw new RangeError("Cannot add negative number of stored chassis");
-        const current : Count<string | Mech>|undefined = this.myStored.get(model);
+        const current: Count<string | Mech> | undefined = this.myStored.get(model);
         if (current) {
             this.setStored(model, current.add(delta).count);
         } else {
@@ -246,7 +352,7 @@ export class SimpleStorage implements MechStorage {
      */
     removeStored(model: string, delta: number) {
         if (delta < 0) throw new RangeError("Cannot remove negative number of stored chassis");
-        const current : Count<string | Mech>|undefined = this.myStored.get(model);
+        const current: Count<string | Mech> | undefined = this.myStored.get(model);
         if (current) {
             if (delta > current.valueOf()) {
                 throw new RangeError("Cannot reduce chassic count below zero");
@@ -306,7 +412,11 @@ export class SimpleStorage implements MechStorage {
             }
         });
     }
-    bays: MechBay[] = [];
+    get bays(): MechBay[] {
+        return new Proxy(this.myBays, {
+
+        });
+    }
     get isComplete(): boolean {
         return this.myParts.entries().every((entry: [string, Count<string | Mech>]) => (typeof entry[1]?.value !== "string")) &&
             this.myStored.entries().every((entry: [string, Count<string | Mech>]) => (typeof entry[1]?.value !== "string"));
