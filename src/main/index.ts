e2dfb22af41d5b328b37de11db4fd8e5b9aecf1c
Antti Kautiainen <antti@kautiainen.com>
2025-02-26 16:03:51 +0200

    feat: main library importing mech library content
    Added count methods to the main library.
    
    Changes to be committed:
    	modified:   src/main/index.ts
        - feat: method createCount
        - feat: method createIntCount
        - feat: interface Count

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,14 +6,24 @@
 
 import { Logger, ConsoleLogger } from "./logger.js";
 
+import { Count, createCount, createIntCount } from "./mech.js";
+
+export interface BTechGameModelLib {
+    createCount: typeof createCount,
+    createIntCount: typeof createIntCount
+}
+
 /**
  * 
  * @param logger 
  */
-export default async function BTechGameModel(logger: Logger|undefined = undefined) {
+export default async function BTechGameModel(logger: Logger|undefined = undefined): Promise<BTechGameModelLib> {
     if (logger) {
         logger.log("Battletech aid module loaded successfully");
     }
+    return {
+        createCount, createIntCount
+    };
 }
 
 BTechGameModel(new ConsoleLogger());
